be6417f83404abb8a62e7418ad4bcfbbc7e5f537
Vladlen <vladlen@vozhzhaev.ru>
2021-05-13 18:21:04 +0300

    Добавлен обработчик событий на кнопку "Да"

--- firstApp/app/src/main/java/com/example/firstapp/MainActivity.java
+++ firstApp/app/src/main/java/com/example/firstapp/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button yesBtn;
+    private Button noBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        yesBtn = findViewById(R.id.btnYes);
+
+        yesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,R.string.correct,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
